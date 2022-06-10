6c40c2a1449d68aca49ea19a8fcc98bc69ba6dc6
sopyan04 <rifai.sopyan04@gmail.com>
2022-06-10 20:59:26 +0700

    menambahkan file c++

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cout << "GAME MEMBALIKAN ANGKA" << "\n \n";
+    
+    int a, b;
+
+    cout << "masukkan angka pertama: ";
+    cin >> a;
+
+    cout << "masukkan angka kedua: ";
+    cin >> b;
+
+    cout << b << ", " << a << "\n \n";
+
+}
